d7c1404aa09ecddda3853b70ff1600a4aa276218
Nathan BENHADOUCHE <nbenhado@e2r5p13.42.fr>
2021-06-30 23:23:35 +0200

    derniers commit de la soiree

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:19:01 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/06/29 23:35:38 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/06/30 23:19:40 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 void	ft_print_alphabet(void)
 {
-	char alph;
+	char	alph;
 
 	alph = 96;
 	while (++alph <= 'z')
 		write(1, &alph, 1);
 	return ;
-}
-
-int main()
-{
-	ft_print_alphabet();
-	return 0;
 }
