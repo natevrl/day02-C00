a80c8c464d8c818a8f5456d6e6a1e28e808d8257
Nathan BENHADOUCHE <nbenhado@e2r5p13.42.fr>
2021-06-30 21:42:40 +0200

    rename exos

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,49 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ttttttttteeeesttttttt.c                            :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/30 16:56:48 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/06/30 21:25:29 by nbenhado         ###   ########.fr       */
+/*   Created: 2021/06/30 20:56:23 by nbenhado          #+#    #+#             */
+/*   Updated: 2021/06/30 21:01:57 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_print_comb2(void)
-{
-	int	a;
-	int	b;
-
-	a = 00;
-	b = 01;
-	while (a < 99)
-	{
-		ft_putchar(a / 10 + 48);
-		ft_putchar(a % 10 + 48);
-		ft_putchar(' ');
-		ft_putchar(b / 10 + 48);
-		ft_putchar(b % 10 + 48);
-		ft_putchar(',');
-		b++;
-		if (b > 99)
-		{
-			a++;
-			b = a + 1;
-		}
-	}
-}
-
-int	main(void)
-{
-	ft_print_comb2();
-	return (0);
-}
+void ft_putnbr(int nb)
