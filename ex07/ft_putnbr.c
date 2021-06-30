0a24b110f8c02751a2a74df0881a57dbf4775d75
Nathan BENHADOUCHE <nbenhado@e2r5p13.42.fr>
2021-06-30 23:16:27 +0200

    exo7 en cours, logique comprise

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,10 +6,37 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 20:56:23 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/06/30 21:01:57 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/06/30 23:15:32 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void ft_putnbr(int nb)
+{
+	int i;
+	i = nb;
+
+	if(i > 9){
+		ft_putnbr(i / 10);
+		ft_putchar(i % 2+48);
+	}
+	else
+	{
+		ft_putchar(i+48);
+	}
+	
+}
+
+
+
+int main(void)
+{
+	ft_putnbr(556);
+	return (0);
+}
