e646d6eb1619e0f098cda7062b7c94a8ea36b23c
Nathan BENHADOUCHE <nbenhado@e3r1p6.42.fr>
2021-07-01 11:12:29 +0200

    finit exo7

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,37 +6,42 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 20:56:23 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/06/30 23:15:32 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/07/01 11:09:50 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-	int i;
-	i = nb;
-
-	if(i > 9){
-		ft_putnbr(i / 10);
-		ft_putchar(i % 2+48);
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb %= 10);
 	}
 	else
 	{
-		ft_putchar(i+48);
+		ft_putchar(nb + 48);
 	}
-	
 }
 
-
-
-int main(void)
+int	main(void)
 {
-	ft_putnbr(556);
+	ft_putnbr(-2147483649);
 	return (0);
 }
